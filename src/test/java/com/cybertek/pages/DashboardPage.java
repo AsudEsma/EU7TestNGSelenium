a75dab0c067ea833e94eb784740c6234e196b54f
AsudEsma <asesozdemir@gmail.com>
2022-01-27 23:21:32 +0300

    PageSubtitleTest added.

--- src/test/java/com/cybertek/pages/DashboardPage.java
+++ src/test/java/com/cybertek/pages/DashboardPage.java
@@ -1,6 +1,5 @@
 package com.cybertek.pages;
 
 public class DashboardPage extends BasePage {
-
-
+    // No need to explicitly write constructor, because it will use its parent constructor.
 }
--- src/test/java/com/cybertek/tests/day13_pom/PageSubtitleTest.java
+++ src/test/java/com/cybertek/tests/day13_pom/PageSubtitleTest.java
@@ -1,15 +1,26 @@
 package com.cybertek.tests.day13_pom;
 
 import com.cybertek.pages.BasePage;
+import com.cybertek.pages.DashboardPage;
 import com.cybertek.pages.LoginPage;
+import com.cybertek.tests.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PageSubtitleTest extends BasePage {
+public class PageSubtitleTest extends TestBase {
 
     @Test
     public void test1() {
         LoginPage loginPage = new LoginPage();
         loginPage.loginAsDriver();
+
+        String expectedSubtitle = "Quick Launchpad";
+        DashboardPage dashboardPage = new DashboardPage();
+        String actualSubtitle = dashboardPage.getPageSubTitle();
+
+        Assert.assertEquals(expectedSubtitle, actualSubtitle, "Verify subtitles are a match");
+
+        dashboardPage.navigateToModule("Activities", "Calendar Events");
 
     }
 }
