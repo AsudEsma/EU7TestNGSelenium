e91753b6688477119aa88a62455c3da796538b66
AsudEsma <asesozdemir@gmail.com>
2022-02-02 17:39:34 +0300

    Contact info test is here

--- src/test/java/com/cybertek/tests/day15_review/VerifyContactInfoTest.java
+++ src/test/java/com/cybertek/tests/day15_review/VerifyContactInfoTest.java
@@ -1,10 +1,12 @@
 package com.cybertek.tests.day15_review;
 
+import com.cybertek.pages.ContactInfoPage;
 import com.cybertek.pages.ContactsPage;
 import com.cybertek.pages.DashboardPage;
 import com.cybertek.pages.LoginPage;
 import com.cybertek.tests.TestBase;
 import com.cybertek.utilities.ConfigurationReader;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class VerifyContactInfoTest extends TestBase {
@@ -20,8 +22,20 @@ public class VerifyContactInfoTest extends TestBase {
 
         ContactsPage contactsPage = new ContactsPage();
         contactsPage.waitUntilLoaderScreenDisappear();
+
+        contactsPage.waitUntilLoaderScreenDisappear();
         contactsPage.getContactEmail("mbrackstone9@example.com").click();
+
+        ContactInfoPage contactInfoPage = new ContactInfoPage();
+        contactInfoPage.waitUntilLoaderScreenDisappear();
+
+        String expectedFullName = "Mariam Brackstone";
+        String actualFullName = contactInfoPage.fullName.getText();
+
+        Assert.assertEquals(actualFullName,expectedFullName,"verify fullname");
+
+        Assert.assertEquals(contactInfoPage.email.getText(),"mbrackstone9@example.com","Verify email");
+
+        Assert.assertEquals(contactInfoPage.phoneNumber.getText(),"+18982323434","verify phone number");
     }
-
-
 }
