a492d1840f8219cbfdf4b48d6c12893885b67543
AsudEsma <asesozdemir@gmail.com>
2022-02-05 00:27:28 +0300

    ExcelUtilDemo is created.

---
+++ src/test/java/com/cybertek/tests/day16_ddf/ExcelUtilDemo.java
@@ -0,0 +1,41 @@
+package com.cybertek.tests.day16_ddf;
+
+import com.cybertek.utilities.ExcelUtil;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class ExcelUtilDemo {
+
+    @Test
+    public void readExcelFile() {
+
+        // Create an object from ExcelUtil
+        // It accepts two arguments:
+        // Argument1: Location of the file (path)
+        // Argument2: Sheet that we want to open (sheetName)
+
+        ExcelUtil qa3short = new ExcelUtil("src/test/resources/Vytracktestdata.xlsx", "QA3-short");
+        System.out.println("qa3short.rowCount() = " + qa3short.rowCount());
+        System.out.println("qa3short.columnCount() = " + qa3short.columnCount());
+
+        // While sending nums, it accepts the numbers starting from 0
+        System.out.println("qa3short.getCellData(11,3) = " + qa3short.getCellData(11, 0));
+        System.out.println("qa3short.getCellData(11,2) = " + qa3short.getCellData(11, 1));
+        System.out.println("qa3short.getCellData(11,3) = " + qa3short.getCellData(11, 2));
+        System.out.println("qa3short.getCellData(11,4) = " + qa3short.getCellData(11, 3));
+
+        System.out.println("qa3short.getColumnsNames() = " + qa3short.getColumnsNames());
+
+        // Get all data in List of Maps
+        List<Map<String, String>> dataList = qa3short.getDataList();
+        for (Map<String, String> oneRow : dataList) {
+            System.out.println("oneRow = " + oneRow);
+        }
+
+        System.out.println("dataList.get(2) = " + dataList.get(2));
+        System.out.println("dataList.get(2).get(\"firstname\") = " + dataList.get(2).get("firstname"));
+
+    }
+}
